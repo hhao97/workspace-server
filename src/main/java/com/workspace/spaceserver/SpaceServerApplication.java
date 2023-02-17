73ef70d8ad7d3532541a8075003b0ce7fca1c508
黄浩 <huanghao@sz-rcd.com>
2023-02-17 16:38:57 +0800

    init

---
+++ src/main/java/com/workspace/spaceserver/SpaceServerApplication.java
@@ -0,0 +1,16 @@
+package com.workspace.spaceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huanghao
+ */
+@SpringBootApplication
+public class SpaceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaceServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workspace/spaceserver/SpaceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspace.spaceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
